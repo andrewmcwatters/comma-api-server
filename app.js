74a78dc60d55fdd62a0d4a4929381a65ac9ae205
Andrew McWatters <mcwattersandrew@gmail.com>
2024-04-13 21:29:55 -0700

    Require athena.js in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('./athena');
 
 app.use(logger('dev'));
 app.use(express.json());
