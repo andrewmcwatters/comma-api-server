d942b8748799ec25ac975c60d19a83b03132854b
Andrew McWatters <mcwattersandrew@gmail.com>
2024-04-18 08:50:32 -0700

    Update GET Upload URL status code to 412

--- routes/openpilot.js
+++ routes/openpilot.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Upload URL. */
 router.get('/v1.4/:dongle_id/upload_url/', function(req, res, next) {
-  res.status(501).json({ error: "Not Implemented" });
+  res.status(412).json({ error: "Upload not supported" });
 });
 
 module.exports = router;
