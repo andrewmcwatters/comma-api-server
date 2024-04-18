eace899486055f2f34c99780f97141425150fd6e
Andrew McWatters <mcwattersandrew@gmail.com>
2024-04-18 08:41:59 -0700

    Update GET Upload URL response to return JSON error message

--- routes/openpilot.js
+++ routes/openpilot.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Upload URL. */
 router.get('/v1.4/:dongle_id/upload_url/', function(req, res, next) {
-  res.sendStatus(501);
+  res.status(501).json({ error: "Not Implemented" });
 });
 
 module.exports = router;
