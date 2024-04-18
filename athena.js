3d886754582e28e8160eb6e07f26b879445a20a4
Andrew McWatters <mcwattersandrew@gmail.com>
2024-04-18 07:54:05 -0700

    https://github.com/websockets/ws/tree/d343a0cf7bba29a4e14217cb010446bec8fdf444?tab=readme-ov-file#how-to-detect-and-close-broken-connections

--- athena.js
+++ athena.js
@@ -1,13 +1,32 @@
 const { WebSocketServer } = require('ws');
 
+function heartbeat() {
+  this.isAlive = true;
+}
+
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+  ws.isAlive = true;
   ws.on('error', console.error);
+  ws.on('pong', heartbeat);
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
 
   // ws.send('something');
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
+wss.on('close', function close() {
+  clearInterval(interval);
 });
